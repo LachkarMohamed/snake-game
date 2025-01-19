aa9bb2db0ef5c0ad7deba1ba9a549af83571eb3d
MOHAMED LACHKAR <165234827+LachkarMohamed@users.noreply.github.com>
2025-01-19 17:37:27 +0100

    fix map7 obs

--- Assets/script/GameHandler.cs
+++ Assets/script/GameHandler.cs
@@ -248,30 +248,13 @@ public class GameHandler : MonoBehaviour
         // Obstacles for map7
         List<Vector2Int> obstaclePositionsMap7 = new List<Vector2Int>
         {
-            new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0), new Vector2Int(3, 0),
-            new Vector2Int(4, 0), new Vector2Int(5, 0), new Vector2Int(6, 0), new Vector2Int(7, 0),
-            new Vector2Int(8, 0), new Vector2Int(9, 0), new Vector2Int(10, 0), new Vector2Int(11, 0),
-            new Vector2Int(12, 0), new Vector2Int(13, 0), new Vector2Int(14, 0), new Vector2Int(15, 0),
-            new Vector2Int(0, 1), new Vector2Int(0, 2), new Vector2Int(0, 3), new Vector2Int(0, 4),
-            new Vector2Int(0, 5), new Vector2Int(0, 6), new Vector2Int(0, 7), new Vector2Int(0, 8),
-            new Vector2Int(0, 9), new Vector2Int(0, 10), new Vector2Int(0, 11), new Vector2Int(0, 12),
-            new Vector2Int(0, 13), new Vector2Int(0, 14), new Vector2Int(0, 15), new Vector2Int(15, 1),
-            new Vector2Int(15, 2), new Vector2Int(15, 3), new Vector2Int(15, 4), new Vector2Int(15, 5),
-            new Vector2Int(15, 6), new Vector2Int(15, 7), new Vector2Int(15, 8), new Vector2Int(15, 9),
-            new Vector2Int(15, 10), new Vector2Int(15, 11), new Vector2Int(15, 12), new Vector2Int(15, 13),
-            new Vector2Int(15, 14), new Vector2Int(15, 15), new Vector2Int(1, 15), new Vector2Int(2, 15),
-            new Vector2Int(3, 15), new Vector2Int(4, 15), new Vector2Int(5, 15), new Vector2Int(6, 15),
-            new Vector2Int(7, 15), new Vector2Int(8, 15), new Vector2Int(9, 15), new Vector2Int(10, 15),
-            new Vector2Int(11, 15), new Vector2Int(12, 15), new Vector2Int(13, 15), new Vector2Int(14, 15),
-            new Vector2Int(2, 2), new Vector2Int(3, 2), new Vector2Int(4, 2), new Vector2Int(5, 2),
-            new Vector2Int(6, 2), new Vector2Int(7, 2), new Vector2Int(8, 2), new Vector2Int(9, 2),
-            new Vector2Int(10, 2), new Vector2Int(11, 2), new Vector2Int(12, 2), new Vector2Int(13, 2),
-            new Vector2Int(14, 2), new Vector2Int(2, 13), new Vector2Int(3, 13), new Vector2Int(4, 13),
-            new Vector2Int(5, 13), new Vector2Int(6, 13), new Vector2Int(7, 13), new Vector2Int(8, 13),
-            new Vector2Int(9, 13), new Vector2Int(10, 13), new Vector2Int(11, 13), new Vector2Int(12, 13),
-            new Vector2Int(13, 13), new Vector2Int(14, 13), new Vector2Int(7, 7), new Vector2Int(8, 7),
-            new Vector2Int(7, 8), new Vector2Int(8, 8)
-        };
+            new Vector2Int(0, 6), new Vector2Int(1, 6), new Vector2Int(2, 6), new Vector2Int(3, 6), new Vector2Int(4, 6), new Vector2Int(5, 6), new Vector2Int(6, 6), new Vector2Int(7, 6), new Vector2Int(8, 6), new Vector2Int(9, 6), new Vector2Int(10, 6), new Vector2Int(11, 6), new Vector2Int(12, 6), new Vector2Int(13, 6), new Vector2Int(14, 6), new Vector2Int(15, 6),
+            new Vector2Int(9, 5), new Vector2Int(9, 4), new Vector2Int(9, 3), new Vector2Int(9, 2), new Vector2Int(9, 1), new Vector2Int(9, 0),
+            new Vector2Int(0, 9), new Vector2Int(1, 9), new Vector2Int(2, 9), new Vector2Int(3, 9), new Vector2Int(4, 9), new Vector2Int(5, 9), new Vector2Int(6, 9), new Vector2Int(9, 9), new Vector2Int(10, 9), new Vector2Int(11, 9), new Vector2Int(12, 9), new Vector2Int(13, 9), new Vector2Int(14, 9), new Vector2Int(15, 9),
+            new Vector2Int(0, 12), new Vector2Int(0, 13), new Vector2Int(0, 14), new Vector2Int(0, 15), new Vector2Int(1, 15),
+            new Vector2Int(4, 15), new Vector2Int(5, 15), new Vector2Int(6, 15), new Vector2Int(7, 15), new Vector2Int(8, 15), new Vector2Int(9, 15), new Vector2Int(10, 15), new Vector2Int(11, 15), new Vector2Int(12, 15),
+            new Vector2Int(6, 10), new Vector2Int(6, 11), new Vector2Int(6, 12), new Vector2Int(6, 13), new Vector2Int(6, 14),
+        }; 
 
         mapDataDictionary["map7"] = new GameHandlerMapData(
             obstaclePositionsMap7,
